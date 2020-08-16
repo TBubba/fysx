89139d508cb3e14482f1e282cf30c549a0978559
Jesper Gustafsson <darkclow3r@gmail.com>
2020-08-16 23:14:25 +0200

    feat: add stiffness modifier to angle constraint (also fix them)
    * Add "stiff" modifier to angle constraint. This decides how much force
    will be used to enforce the constraint.
    * Fix the angle constraint to work better (still kinda broken).

--- src/constraints/AngleConstraint.ts
+++ src/constraints/AngleConstraint.ts
@@ -1,6 +1,7 @@
 import { IConstraint } from '../interfaces';
 import { IBody } from '../Body';
 import { IVertex } from '../Vertex';
+import { Vec2 } from '../Vec2';
 
 /**
  * Angle Constraint.
@@ -11,13 +12,15 @@ export class AngleConstraint<T extends IBody> implements IConstraint<T> {
   v1: IVertex<T>;
   v2: IVertex<T>;
   angle: number;
+  stiff: number;
 
-  constructor(parent: T, v0: IVertex<T>, v1: IVertex<T>, v2: IVertex<T>, angle: number = 0) {
+  constructor(parent: T, v0: IVertex<T>, v1: IVertex<T>, v2: IVertex<T>, angle = 0, stiff = 0.1) {
     this.parent = parent;
     this.v0 = v0;
     this.v1 = v1;
     this.v2 = v2;
     this.angle = angle;
+    this.stiff = stiff;
   }
 
   solve(): void {
@@ -25,12 +28,15 @@ export class AngleConstraint<T extends IBody> implements IConstraint<T> {
     const p1 = this.v1.position;
     const p2 = this.v2.position;
     
-    const angle = p0.angleTo(p1) + this.angle;
-    const dist = p0.distanceTo(p2);
+    let dif = p0.angleTo(p1) + this.angle - p0.angleTo(p2);
+    if      (dif <= -Math.PI) { dif += Math.PI * 2; }
+    else if (dif >=  Math.PI) { dif -= Math.PI * 2; }
 
-    p2.set(
-      Math.cos(angle) * dist,
-      Math.sin(angle) * dist
-    );
+    dif *= this.stiff * 0.5;
+
+    p1.rotate(p0, -dif);
+    p2.rotate(p0, dif);
+    //p0.rotate(p1, dif);
+    //p0.rotate(p2, -dif);
   }
 }
