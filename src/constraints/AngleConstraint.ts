4c607e88c5354033bfb690309f3502e9da6b1cab
Jesper Gustafsson <darkclow3r@gmail.com>
2020-08-20 03:01:10 +0200

    feat: improve angle constraint (add min/max, add pow, make more robust)

--- src/constraints/AngleConstraint.ts
+++ src/constraints/AngleConstraint.ts
@@ -1,7 +1,6 @@
-import { IConstraint } from '../interfaces';
 import { IBody } from '../Body';
+import { IConstraint } from '../interfaces';
 import { IVertex } from '../Vertex';
-import { Vec2 } from '../Vec2';
 
 /**
  * Angle Constraint.
@@ -11,32 +10,46 @@ export class AngleConstraint<T extends IBody> implements IConstraint<T> {
   v0: IVertex<T>;
   v1: IVertex<T>;
   v2: IVertex<T>;
-  angle: number;
-  stiff: number;
+  min: number;
+  max: number;
+  pow: number;
 
-  constructor(parent: T, v0: IVertex<T>, v1: IVertex<T>, v2: IVertex<T>, angle = 0, stiff = 0.1) {
+  constructor(parent: T, v0: IVertex<T>, v1: IVertex<T>, v2: IVertex<T>, min = 0, max = 0, pow = 0.5) {
     this.parent = parent;
     this.v0 = v0;
     this.v1 = v1;
     this.v2 = v2;
-    this.angle = angle;
-    this.stiff = stiff;
+    this.min = angle(min);
+    this.max = angle(max);
+    this.pow = pow;
   }
 
   solve(): void {
     const p0 = this.v0.position;
     const p1 = this.v1.position;
     const p2 = this.v2.position;
-    
-    let dif = p0.angleTo(p1) + this.angle - p0.angleTo(p2);
-    if      (dif <= -Math.PI) { dif += Math.PI * 2; }
-    else if (dif >=  Math.PI) { dif -= Math.PI * 2; }
 
-    dif *= this.stiff * 0.5;
+    const a1 = angle(p0.angleTo(p1));
+    const a2 = angle(p0.angleTo(p2));
 
-    p1.rotate(p0, -dif);
-    p2.rotate(p0, dif);
-    //p0.rotate(p1, dif);
-    //p0.rotate(p2, -dif);
+    const range = Math.abs(this.max - this.min) * 0.5;
+    const dif = angle(angle(a1 + this.min - range) - a2);
+
+    if (Math.abs(dif) > range) {
+      const rot = (dif - range * sign(dif));
+      p2.rotate(p0,  rot * this.pow);
+      p1.rotate(p0, -rot * (1 - this.pow));
+    }
   }
+}
+
+/** Simplify an angle to a range between [-PI, PI]. */
+function angle(angle: number): number {
+  return (Math.abs(angle) > Math.PI)
+    ? angle + (((angle / (Math.PI * 2)) | 0) - sign(angle)) * (Math.PI * 2)
+    : angle;
+}
+
+function sign(x: number): number {
+  return x ? x < 0 ? -1 : 1 : 0;
 }
